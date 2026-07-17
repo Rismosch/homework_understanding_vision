19eaac34acd389ce59a52274888ea434e593537c
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-07-17 16:27:23 +0200

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod homework_4;
 mod pcg;
 mod rng;
 
+use std::io::Write;
+use std::process::Child;
+
 fn main() {
     println!("running homework 3...");
     homework_3::run();
@@ -15,4 +18,26 @@ fn main() {
     homework_4::run();
 
     println!("done!");
+}
+
+fn exit_gnuplot(mut gnuplot: Child) {
+    let stdin = gnuplot.stdin.as_mut().expect("stdin to exist");
+    writeln!(stdin, "exit").unwrap();
+    let timeout = std::time::Duration::from_secs(5);
+    let start = std::time::Instant::now();
+    loop {
+        if let Some(exit_status) = gnuplot.try_wait().unwrap() {
+            println!("gnuplot exited with exit status: {}", exit_status);
+            break;
+        }
+
+        let now = std::time::Instant::now();
+        let diff = now - start;
+        if diff > timeout {
+            println!("failed to wait for gnuplot to exit in {:?}", timeout);
+            break;
+        }
+
+        std::thread::yield_now();
+    }
 }
